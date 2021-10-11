15d5ec9c2437074ca330dff64a712b6e250b5550
Ilias <Iliastakassa@hotmail.com>
2021-10-11 12:18:09 +0200

    Python

--- public_html/init.py
+++ public_html/init.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app=Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
+    if request.method == 'POST' and 'UserName' in request.form and 'Password' in request.form:
+        return redirect("http://www.google.com/", code=302)
     return render_template("index.html")
